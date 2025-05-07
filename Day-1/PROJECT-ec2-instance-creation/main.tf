5cd208b56d5287f3d17556ca34392ea25be5b8be
dhavall <dhavalrathod23kool@gmail.com>
2025-05-06 19:46:03 -0500

    main.tf updated

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    ami = "ami-09cb80360d5069de4"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+    subnet_id = "subnet-02625c375db89ab38"
+    key_name = "ec2-instance-keypair"
 }
